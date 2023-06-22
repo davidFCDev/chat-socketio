da40a907d6861b156fe778b62559f8ef6e33aabd
David Fernández <davidfernandezcomesana@gmail.com>
2023-06-22 19:50:07 +0200

    añadido frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
-function App() {
-  const [count, setCount] = useState(0)
+// const socket = io("http://localhost:3001");
+const socket = io("/");
+
+export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("message", receiveMessage)
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages(state => [message, ...state]);
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages(state => [newMessage, ...state]);
+    setMessage("");
+    socket.emit("message", newMessage.body);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat React</h1>
+        <input
+          name="message"
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          value={message}
+          autoFocus
+        />
 
-export default App
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-cyan-500 ml-auto" : "bg-green-500"
+                }`}
+            >
+              <b>{message.from}</b>: {message.body}
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 import express from "express";
 import http from "http";
+import morgan from "morgan";
 import { Server as SocketServer } from "socket.io";
+import { resolve, dirname } from "path";
 
+import cors from "cors";
+
+// Initializations
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+  // cors: {
+  //   origin: "http://localhost:3000",
+  // },
+});
+
+// Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(resolve("frontend/dist")));
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (body) => {
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(8),
+    });
+  });
+});
 
 server.listen(4000);
-console.log("Server on port", 4000);
+console.log(`server on port 4000`);
